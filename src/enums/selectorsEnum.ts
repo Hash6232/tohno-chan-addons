0bcf1d11910613c07be401f5594fc971418f6076
Hash6232 <lagged9071@proton.me>
2025-04-23 19:02:29 +0200

    Use simpler id selectors

--- src/enums/selectorsEnum.ts
+++ src/enums/selectorsEnum.ts
@@ -7,7 +7,7 @@ export const enum QuickreplySelectorsEnum {
   ROOT = "#quick-reply",
   CLOSE = QuickreplySelectorsEnum.ROOT + " a.close-btn",
   TEXTAREA = QuickreplySelectorsEnum.ROOT + " textarea[name='body']",
-  UPLOAD_ROW = QuickreplySelectorsEnum.ROOT + " tr#upload",
-  FILEINPUT = QuickreplySelectorsEnum.ROOT + " input#upload_file",
+  UPLOAD_ROW = QuickreplySelectorsEnum.ROOT + " #upload",
+  FILEINPUT = QuickreplySelectorsEnum.ROOT + " #upload_file",
   FORMATTING_ROW = QuickreplySelectorsEnum.ROOT + " tr:has(input[name='b'])",
 }
