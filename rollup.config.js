3a39c69e18f9c79981ffd71498f99ad481c143e6
Hash6232 <lagged9071@proton.me>
2025-04-23 18:57:04 +0200

    Formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,45 @@
 import fs from "fs";
 import path from "path";
-import { defineConfig } from 'rollup';
-import typescript from '@rollup/plugin-typescript';
-import alias from '@rollup/plugin-alias';
-import scss from 'rollup-plugin-scss';
-import terser from '@rollup/plugin-terser';
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import alias from "@rollup/plugin-alias";
+import scss from "rollup-plugin-scss";
+import terser from "@rollup/plugin-terser";
 
-const inputFiles = fs.readdirSync("src")
-  .filter((file) => file.endsWith('.ts')) 
+const inputFiles = fs
+  .readdirSync("src")
+  .filter((file) => file.endsWith(".ts"))
   .map((file) => path.join("src", file));
 
 const outputs = inputFiles.map((inputFile) => ({
-    input: inputFile,
-    output: {
-        file: path.join('dist', path.basename(inputFile, ".ts") + ".js"),
-        format: 'iife',
-        sourcemap: true
-    },
-    plugins: [
-      typescript(),
-      alias({
-        entries: [
-          { find: '@enums', replacement: './src/enums' },
-          { find: '@features', replacement: './src/features' },
-          { find: '@utils', replacement: './src/utils' }
-        ]
-      }),
-      scss({
-        output: false, 
-        insert: true, 
-        outputStyle: 'compressed',
-        processor: (css) => {
-          /* Prepend script name for reference */
-          const filename = path.basename(inputFile, '.ts');
-          return `/* ${filename} */\n` + css
-        },
-        silenceDeprecations: ["legacy-js-api"]
-      }),
-      terser()
-    ],
+  input: inputFile,
+  output: {
+    file: path.join("dist", path.basename(inputFile, ".ts") + ".js"),
+    format: "iife",
+    sourcemap: true,
+  },
+  plugins: [
+    typescript(),
+    alias({
+      entries: [
+        { find: "@enums", replacement: "./src/enums" },
+        { find: "@features", replacement: "./src/features" },
+        { find: "@utils", replacement: "./src/utils" },
+      ],
+    }),
+    scss({
+      output: false,
+      insert: true,
+      outputStyle: "compressed",
+      processor: (css) => {
+        /* Prepend script name for reference */
+        const filename = path.basename(inputFile, ".ts");
+        return `/* ${filename} */\n` + css;
+      },
+      silenceDeprecations: ["legacy-js-api"],
+    }),
+    terser(),
+  ],
 }));
 
 export default defineConfig(outputs);
