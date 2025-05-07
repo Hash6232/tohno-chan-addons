dede852d4472640997246915de1c6a95719fb136
Hash6232 <lagged9071@proton.me>
2025-05-07 17:50:45 +0200

    Organize and refactor allowed extensions per category

--- src/config.ts
+++ src/config.ts
@@ -1,45 +1,47 @@
 const Config = {
-  allowed_files: {
-    bmp: "image/bmp",
-    flac: "audio/flac",
-    // flv: "video/x-flv",
-    gif: "image/gif",
-    jpg: "image/jpeg",
-    jpeg: "image/jpeg",
-    mp3: "audio/mpeg",
-    mp4: "video/mp4",
-    // pdf: "application/pdf",
-    png: "image/png",
-    // rar: "application/x-rar-compressed",
-    // swf: "application/x-shockwave-flash",
-    // txt: "text/plain",
-    webm: "video/webm",
-    // webp: "image/webp",
-    // zip: "application/zip",
+  allowed_ext: {
+    image: {
+      bmp: "image/bmp",
+      gif: "image/gif",
+      jpg: "image/jpeg",
+      jpeg: "image/jpeg",
+      png: "image/png",
+      // webp: "image/webp",
+    },
+    video: {
+      // flv: "video/x-flv",
+      mp4: "video/mp4",
+      webm: "video/webm",
+    },
+    audio: {
+      flac: "audio/flac",
+      mp3: "audio/mpeg",
+    },
+    other: {
+      // pdf: "application/pdf",
+      // rar: "application/x-rar-compressed",
+      // swf: "application/x-shockwave-flash",
+      // txt: "text/plain",
+      // zip: "application/zip",
+    },
   },
   max_filesize: 2500,
   max_image: 1,
 } as const;
 
 export namespace AllowedTypes {
-  export type Files = typeof Config.allowed_files;
-  export type Extensions = keyof Files;
-  export type Mimes = Files[Extensions];
-  export type ImageExtensions = keyof Pick<Files, "bmp" | "gif" | "jpg" | "jpeg" | "png" >; // | "webp"
-  export type ImageMimes = Files[ImageExtensions];
-  export type ImageFiles = Pick<Files, ImageExtensions>;
-  export type VideoExtensions = keyof Pick<Files, "mp4" | "webm">; // "flv" |
-  export type VideoMimes = Files[VideoExtensions];
-  export type VideoFiles = Pick<Files, VideoExtensions>;
-  export type AudioExtensions = keyof Pick<Files, "flac" | "mp3">;
-  export type AudioMimes = Files[AudioExtensions];
-  export type AudioFiles = Pick<Files, AudioExtensions>;
-  export type MediaExtensions = ImageExtensions | VideoExtensions | AudioExtensions; // | "swf"
-  export type MediaMimes = Files[MediaExtensions];
-  export type MediaFiles = Pick<Files, MediaExtensions>;
-  export type OtherExtensions = keyof Omit<Files, MediaExtensions>;
-  export type OtherMimes = Files[OtherExtensions];
-  export type OtherFiles = Pick<Files, OtherExtensions>;
+  export type ImageExtensions = keyof typeof Config.allowed_ext.image;
+  export type VideoExtensions = keyof typeof Config.allowed_ext.video;
+  export type AudioExtensions = keyof typeof Config.allowed_ext.audio;
+  export type MediaExtensions = ImageExtensions | VideoExtensions | AudioExtensions;
+  export type OtherExtensions = keyof typeof Config.allowed_ext.other;
+  export type ImageMimes = typeof Config.allowed_ext.image[ImageExtensions];
+  export type VideoMimes = typeof Config.allowed_ext.video[VideoExtensions];
+  export type AudioMimes = typeof Config.allowed_ext.audio[AudioExtensions];
+  export type MediaMimes = ImageMimes | VideoMimes | AudioMimes;
+  export type OtherMimes = typeof Config.allowed_ext.other[OtherExtensions];
+  export type AllExtensions = MediaExtensions | OtherExtensions;
+  export type AllMimes = MediaMimes | OtherMimes;
 }
 
 export default Config;
